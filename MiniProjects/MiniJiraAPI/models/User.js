3a4aec092b6d4baaded2930c6a868d7cf4cd1694
Aniket Vyavahare <aniketvyavahare89301@gmail.com>
2026-04-07 19:00:29 +0530

    Initialising MiniGira API

---
+++ MiniProjects/MiniJiraAPI/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.model(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: [true, "Email is required "],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ MiniProjects/MiniJiraAPI/models/task.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const taskScehma = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["to-do", "review", "in-progress", "completed"],
+      default: "to-do",
+    },
+    deadline: {
+      type: Date,
+    },
+    Board: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Board",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Task", taskScehma);
