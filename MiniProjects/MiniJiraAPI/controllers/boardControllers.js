791bea306265279732180ebe3e736afe273cead5
Aniket Vyavahare <aniketvyavahare89301@gmail.com>
2026-04-08 00:38:26 +0530

    Added Controller Logic

--- MiniProjects/MiniJiraAPI/controllers/boardControllers.js
+++ MiniProjects/MiniJiraAPI/controllers/boardControllers.js
@@ -71,6 +71,106 @@ const getBoardById = async (req, res) => {
     });
   }
 };
+const updateBoard = async (req, res) => {
+  const { title } = req.body;
+  try {
+    const userId = req.user.id;
+    const boardId = req.params.id;
+    const board = await Board.findById(boardId);
+    if (!board) {
+      return res.status(404).json({
+        msg: "Requested Board Does not exist",
+      });
+    }
+    if (board.owner.toString() !== userId) {
+      return res.status(403).json({
+        msg: `You are not Authorized to Acess this Board .`,
+      });
+    }
+    if (title) board.title = title;
+    const updatedBoard = await board.save();
+    res.status(200).json({
+      msg: `Board Updated Successfuly`,
+      updatedBoard,
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: `Server Error Occured`,
+      Error: err.message,
+    });
+  }
+};
+const addMember = async (req, res) => {
+  const { UserId } = req.body;
+  const boardId = req.params.id;
+  try {
+    const board = await Board.findById(boardId);
+    if (!board) {
+      return res.status(404).json({
+        msg: "Requested Board Does not Exist ",
+      });
+    }
+    if (board.owner.toString() !== req.user.id) {
+      return res.status(403).json({
+        msg: `You are not Authorised to view this baord `,
+      });
+    }
+    const alreadyMember = board.members.some(
+      (member) => member.toString() === UserId,
+    );
+    if (alreadyMember) {
+      return res.status(200).json({
+        msg: `User id:${UserId} is already member of the Board .`,
+      });
+    }
+    board.members.push(UserId);
+    await board.save();
+    res.status(200).json({
+      msg: "Member added to Board member Successfully .",
+      members: board.members,
+    });
+  } catch (error) {
+    res.status(500).json({
+      Msg: "Some Error Occured",
+      ERROR: error.message,
+    });
+  }
+};
+const removeMember = async (req, res) => {
+  const boardId = req.params.id;
+  const userToremove = req.params.userId;
+  try {
+    const board = await Board.findById(boardId);
+    if (!board) {
+      return res.status(404).json({
+        msg: "Requested Board Does not Exist",
+      });
+    }
+    if (board.owner.toString() !== req.user.id) {
+      return res.status(403).json({
+        msg: "Only Board Owner Can Remove members",
+      });
+    }
+    if (board.owner.toString() === userToremove) {
+      return res.status(400).json({
+        msg: "Owner of the Board cannot be removed ",
+      });
+    }
+    board.members = board.members.filter(
+      (member) => member.toString() !== userToremove,
+    );
+    await board.save();
+    res.status(200).json({
+      msg: "Board Member removed successfully",
+      Members: board.members,
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Sever Error",
+      Error: error.message,
+    });
+  }
+};
 module.exports = {
   createBoard,
   getBoard,
